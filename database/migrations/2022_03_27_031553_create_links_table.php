34f72055795e9f46ad842cd82f61975aff1013c1
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-03-28 20:58:15 -0400

    Rename name to username in User Model

--- database/migrations/2022_03_27_031553_create_links_table.php
+++ database/migrations/2022_03_27_031553_create_links_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('links', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->string('name');
+            $table->string('username');
             $table->text('link');
             $table->timestamps();
         });
